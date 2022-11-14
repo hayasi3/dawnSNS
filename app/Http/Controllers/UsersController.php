86acbc53fc0ad046dd3ae35c8c6737051cb7f71d
林　宏紀 <hiroki1051@gmail.com>
2022-11-14 16:55:44 +0900

    ユーザ検索書き直し

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,16 @@ use Auth;
 class UsersController extends Controller
 {
     //
+    public function index(Request $request){
+        DB::table('posts')
+        ->insert([
+            'user_id'=>Auth::id(),
+            'posts'=>$request->input('newpost'),
+            'created_at'=>now(),
+        ]);
+        return view('posts.index');
+    }
+
     public function profile(){
         $user = DB::table('users')
         ->where('id',Auth::id())
