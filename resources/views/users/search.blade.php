2b491a9941c4239d545ec919f5be67601691cd7a
林　宏紀 <hiroki1051@gmail.com>
2022-11-14 16:42:05 +0900

    ユーザ検索作成中

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.login')
 
 @section('content')
-
+<form action="/post/search" method="post">
+    @csrf
+    <input type="text" name="search" placeholder="ユーザ名">
+    <input type="image" src="/images/post.png" alt="送信">
+</form>
 
 
 @endsection
