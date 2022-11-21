35b6a00e4ac27d9a14d89ae831866b19818ab9bb
林　宏紀 <hiroki1051@gmail.com>
2022-11-21 17:14:31 +0900

    フォローする、フォローをはずすボタンの設置

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -11,6 +11,19 @@
     <p>{{ $user -> username }}</p>
 @empty
     <p>結果はありませんでした</p>
+    <img src="/images/edit.png" alt="更新">
 @endforelse
+
+@foreach($users as $user)
+<form method="post">
+    @csrf
+    <input type="submit" name="Follow1" value="フォローする">
+  </form>
+
+  <form method="post">
+    @csrf
+    <input type="submit" name="Follow2" value="フォローをはずす">
+  </form>
+@endforeach
 
 @endsection
