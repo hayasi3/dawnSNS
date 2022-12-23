4d64996adcc614f845bc124fdf3f5179cc69f6e2
林　宏紀 <hiroki1051@gmail.com>
2022-12-23 11:39:34 +0900

    自身のプロフォール inputタグでの記載

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.login')
 
 @section('content')
+UserName
+<input type="text" name="name" value="{{$user->username}}">
+MailAdress
+<input type="text" name="email" value="{{$user->mail}}">
+Passward
+<input class="form-control" id="inputPassword" placeholder="パスワード" name="inputPassword" type="password" value="●●●●●●●">
+new Passward
+<input class="form-control" id="inputPassword" placeholder="パスワード" name="inputPassword" type="password" value="●●●●●●●">
 
 @endsection
