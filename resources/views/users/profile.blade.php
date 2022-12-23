d02f65a0e29443c0bbf889dae0a05c3ca37e5816
林　宏紀 <hiroki1051@gmail.com>
2022-12-23 13:09:52 +0900

    プロフィールのblade一応完成

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,12 +2,23 @@
 
 @section('content')
 UserName
-<input type="text" name="name" value="{{$user->username}}">
+{{Form::text('inputName',$user->username, ['class' => 'form-control', 'id' => 'inputName'])}}
+<br>
 MailAdress
-<input type="text" name="email" value="{{$user->mail}}">
+{{Form::text('inputName',$user->mail, ['class' => 'form-control', 'id' => 'inputName'])}}
+<br>
 Passward
-<input class="form-control" id="inputPassword" placeholder="パスワード" name="inputPassword" type="password" value="●●●●●●●">
+{{Form::password('inputPassword',['class' => 'form-control','id' => 'inputPassword','placeholder' => '●●●●●●●'])}}
+<br>
 new Passward
-<input class="form-control" id="inputPassword" placeholder="パスワード" name="inputPassword" type="password" value="●●●●●●●">
+{{Form::password('inputPassword',['class' => 'form-control','id' => 'inputPassword','placeholder' => '●●●●●●●'])}}
+<br>
+Bio
+{{Form::textarea('textareaRemarks', null, ['class' => 'form-control', 'id' => 'textareaRemarks', 'placeholder' => '自己紹介', 'rows' => '2'])}}
+<br>
+Icon image
+{{Form::file('image', ['class'=>'custom-file-input','id'=>'fileImage'])}}
+<br>
+<input type="submit" name="update" value="更新">
 
 @endsection
